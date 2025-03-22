8ca90d955007a69fb77ac12bda5621575e76d657
WELL <wellersonloss@gmail.com>
2025-03-22 02:52:20 -0300

    Atuzand arquivo py

--- codigo_execucao/d2.py
+++ codigo_execucao/d2.py
@@ -2,3 +2,7 @@
 ##Agora vamos solicitar uma string e um número inteiro como entrada.
 ##Depois teremos que retornar a string repetida o número de vezes
 ##informado.
+
+string = input (m)
+numero = int(input("m"))
+print((string + ' ') * numero)
--- codigo_execucao/d3.py
+++ codigo_execucao/d3.py
@@ -1,3 +1,16 @@
 ##3 - Operações Matemáticas Simples
 ##Vamos solicitar como entrada dois números e 
 ##depois vamos realizar uma operação simples entre eles.
+
+num1 = int(input(""))
+num2 = int(input(""))
+
+operacao = input("Digite a operaçao que deseja realizar (+,-,*,/): ")
+if operacao == '+':
+print(num1 + num2)
+
+elif operacao == '-':
+print(abs(num1 - num2))
+
+else:
+print("Operação inválida")
